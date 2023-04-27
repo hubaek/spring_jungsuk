6039a5ddb2aa4b9058510f3d88822fcf44b8f5ca
hubaek <zjfl3122@naver.com>
2023-04-28 01:09:56 +0900

    SRP - 입력부분 생략화 String year, month, day => int year, month, day - main 메서드의 매개변수로 넣음

--- src/main/java/com/fastcampus/ch2/YoilTeller.java
+++ src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -12,19 +12,21 @@ import java.util.Calendar;
 @Controller
 public class YoilTeller {
     @RequestMapping("/getYoil")
-    public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // 1. 입력
-        String year = request.getParameter("year");
-        String month = request.getParameter("month");
-        String day = request.getParameter("day");
-
-        int yyyy = Integer.parseInt(year);
-        int mm = Integer.parseInt(month);
-        int dd = Integer.parseInt(day);
+    public void main(int year, int month, int day,
+            HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 1. 입력 - main메서드 매개변수 -> String year, String month, String day 적으면 안 적어줘도 됨.
+        // 입력은 request.getParemeter로 단순하기 때문에 간략하게 생략 가능
+//        String year = request.getParameter("year");
+//        String month = request.getParameter("month");
+//        String day = request.getParameter("day");
+        // main 메서드 매개변수를 String->int로 바꿔주면 스프링이 알아서 값을 읽고 변환(parseInt)해줌
+//        int yyyy = Integer.parseInt(year);
+//        int mm = Integer.parseInt(month);
+//        int dd = Integer.parseInt(day);
 
         // 2. 처리
         Calendar cal = Calendar.getInstance();
-        cal.set(yyyy, mm - 1, dd);
+        cal.set(year, month - 1, day);
 
         int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
         char yoil = " 일월화수목금토".charAt(dayOfWeek);
