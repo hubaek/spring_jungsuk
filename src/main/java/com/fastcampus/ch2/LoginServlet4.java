d2cfba877592bd0c00bf91fe2bcbff38fe85f460
hubaek <zjfl3122@naver.com>
2023-05-05 12:49:12 +0900

    referer 활용, 응답 조건문 변경 referer 확인을 해서 요청이 들어온 url 확인 조건문으로 board.jsp타고 오는 referer는 redirect login.jsp로 타고 오는 url은 /index.jsp로 넘겨주는 조건 하드 코딩 함

--- src/main/java/com/fastcampus/ch2/LoginServlet4.java
+++ src/main/java/com/fastcampus/ch2/LoginServlet4.java
@@ -18,40 +18,56 @@ public class LoginServlet4 {
             throws IOException, ServletException {
 
         // login.jsp로 부터 id와 pw를 받아온다.
-
         // 1. 입력
         String id = request.getParameter("id");
-        String pwd = request.getParameter("pwd");
 
         // 2. 처리
         // session이 어떻게 작동하는 걸까??
         // 일단. 서버를 킬때마다 쿠키를 보면 JSESSIONID가 기본있고
         // Value만 계속 바뀐다.
-//        HttpSession session = request.getSession();
-////        System.out.println("SesstionID : " + session.getId());
-//        session.setAttribute("id","asdf");
-//
-//        System.out.println("session Get ID : "+session.getAttribute("id"));
-//        // session.getAttribute : "asdf"
 
         // id와 pwd가 일치하면
-        // forward - board.jsp
+        // xxx forward - board.jsp xxx
+        // login버튼으로 왔으면 index / board버튼 - board
         // 일치하지않으면
-        // redirect - login.js인
-        if (id != null) {
-            // session이 어떻게 작동하는 걸까??
-            // 일단. 서버를 킬때마다 쿠키를 보면 JSESSIONID가 기본있고
-            // Value만 계속 바뀐다.
-            HttpSession session = request.getSession();
+        // redirect - login.jsp
+        String referer = request.getHeader("referer");
+        System.out.println("Controller referer 확인 : "+referer);
+
+        if (id != null) {   // id가존재하면 = 로그인이 되었으면
+            HttpSession session = request.getSession(); // 세션을 가져옴
             session.setAttribute("id","asdf");
             System.out.println("session Get ID : "+session.getAttribute("id"));
             // session.getAttribute : "asdf"
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/board.jsp");
-            dispatcher.forward(request, response);
+
+            // board를 클릭해서 login했으면 board.jsp로 가고
+            // login을 클릭해서 login해서 데이터가 넘어오면 index.jsp로 가야함
+            // 근데.. board에서 넘어왔는지 login에서 넘어왔는지 어떻게 확인해야하지..?
+            // referer 활용..
+            // referer을 어떻게 활용할까?
+//            RequestDispatcher dispatcher = request.getRequestDispatcher(referer);
+//            dispatcher.forward(request, response);
+            // board를 클릭하면 board.jsp에서 forward해서 board.jsp주소가 login.jsp타고
+            // 컨트롤러로 넘어오니깐 redirect로 보내주면 되는데
+            // login으로 바로 왔을땐 redirect는 login페이지로 넘어가야한다.
+            if (referer.equals("http://localhost:8080/board.jsp")){
+                response.sendRedirect(referer); // board 눌러서 들어오면
+            } else {
+//                RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+//                dispatcher.forward(request, response);
+                response.sendRedirect("/index.jsp");
+            }
         } else {
             // redirect
             response.sendRedirect("/login.jsp");
         }
 
     }
+    @RequestMapping("/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        response.sendRedirect("/index.jsp");
+    }
+
 }
